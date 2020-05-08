995abbd2d6e3f3d218d84ed19ac779074b9e98c4
Asad <asadalisps@gmail.com>
2020-05-08 05:50:47 +0500

    Intermediate Visualization

--- st_app.py
+++ st_app.py
@@ -2,11 +2,12 @@ import streamlit as st
 import plotly.graph_objects as go
 import pandas as pd
 import COVID19Py
+from plotly.subplots import make_subplots
 
 
 
 
-pandemics=["All","Ebola","Swine Flu","Covid-19"]
+pandemics=["All","Covid-19","Swine Flu","Ebola"]
 
 
 @st.cache
@@ -83,14 +84,17 @@ def make_charts(codes,country_names):
 
 
 
-@st.cache(allow_output_mutation=True)
+@st.cache()
 def load_datasets():
     ebola=pd.read_csv('./ebola/ebola_2014_2016_clean.csv')
     swine_flu=pd.read_csv('./swine_flu/swine_flu.csv')
     mers=pd.read_csv('./mers/weekly_clean.csv')
     sars=pd.read_csv('./sars/sars_2003_complete_dataset_clean.csv')
     covid=pd.read_csv('./covid/covid_19_clean_complete.csv')
-
+    
+    covid19 = COVID19Py.COVID19()
+    covid19 = COVID19Py.COVID19(data_source="jhu")
+    covid_latest=covid19.getLatest()
 
     ebola.rename(columns={  "No. of confirmed cases": "ConfirmedCases",
                 "No. of confirmed deaths": "ConfirmedDeaths",
@@ -98,53 +102,115 @@ def load_datasets():
                 "No. of probable cases": "ProbCases"
             },inplace=True)
 
-
+    swine_flu["Date"]=pd.to_datetime(swine_flu["Update Time"])
+    swine_flu["Date"]=swine_flu["Date"].dt.date
     
 
+
+    print(covid_latest)
+
     
-    return ebola,swine_flu,covid
+    return ebola,swine_flu,covid,covid_latest
 
 
 
 
-def comparison(ebola,swine_flu,covid):
+def comparison(ebola,swine_flu,covid,covid_latest):
+
+    # Preparing Data
+    gb=ebola.groupby(["Date"]).sum()
+    gb2=swine_flu.groupby(["Date"]).sum()
+    gb3=covid.groupby(["Date"]).sum()
+
+
 
     spanish_flu={"Cases":  500000000,
                 "Deaths": 50000000  
     }
-
     hiv={"Cases": 75000000,
         "Deaths": 32000000
     }
-
     names=["Spanish Flu","Hiv","Ebola","Swine Flu","Covid"]
-    cases=[spanish_flu["Cases"],hiv["Cases"],ebola.ConfirmedCases.sum(),swine_flu.Cases.sum(),covid.Confirmed.sum()]
+    cases=[spanish_flu["Cases"],hiv["Cases"],ebola.ConfirmedCases.sum(),swine_flu.Cases.sum(),covid_latest["confirmed"]]
+    deaths=[spanish_flu["Deaths"],hiv["Deaths"],ebola.ConfirmedDeaths.sum(),swine_flu.Deaths.sum(),covid_latest["deaths"]]
+    total_deaths=sum(deaths)
+    total_confirmed=sum(cases)
+    size_deaths=(deaths/total_deaths)*100
+    size_confirmed=(deaths/total_confirmed)*100
 
-    total=spanish_flu["Deaths"]+hiv["Deaths"]+ebola.ConfirmedDeaths.sum()+covid.Deaths.sum()+swine_flu.Deaths.sum()
+    # print(size)
+    # size=[(spanish_flu["Deaths"]/total)*100,(hiv["Deaths"]/total)*100,(ebola.ConfirmedDeaths.sum()/total)*100,
+    #     (swine_flu.Deaths.sum()/total)*100,(covid.Deaths.sum()/total)*100]
+    
+    mortality_rate = [(i /float(j))*100 for i, j in zip(deaths, cases)]
 
 
-    size=[(spanish_flu["Deaths"]/total)*100,(hiv["Deaths"]/total)*100,(ebola.ConfirmedDeaths.sum()/total)*100,(swine_flu.Deaths.sum()/total)*100,(covid.Deaths.sum()/total)*100]
 
-    fig = go.Figure()
+    print (mortality_rate)
+    # Figure1
+    fig=go.Figure()
+    fig.add_trace(go.Scatter(y=gb["ConfirmedDeaths"],x=gb["ConfirmedCases"],name="Ebola"))
+    fig.add_trace(go.Scatter(y=gb2["Deaths"],x=gb2["Cases"],name='Swine Flu'))
+    fig.add_trace(go.Scatter(y=gb3["Deaths"],x=gb3["Confirmed"],name='Covid'))
+    fig.update_layout(title='Confirmed Deaths V Cases',
+                    yaxis_title='Deaths',
+                    xaxis_title='Confirmed')
 
 
-    fig.add_trace(go.Scatter(
+    fig2=make_subplots(rows=1,cols=2,subplot_titles=["Confirmed Cases","Confirmed Deaths"])
+
+    # Bubble Plot Confirmed Cases
+    # fig2 = go.Figure()
+    fig2.add_trace(go.Scatter(
         x=names,
         y=cases,
-        text=['Deaths: '+str(spanish_flu["Deaths"]),'Deaths: '+str(ebola.ConfirmedDeaths.sum()),'Deaths: '+str(covid.Deaths.sum())],
         mode='markers',
         marker=dict(
-            size=size,
-            color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)', 'rgb(255, 65, 104)']
+            size=size_deaths,
+            # color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)', 'rgb(255, 65, 104)']
+        ),
+        name="Confirmed Cases"
+    ),row=1,col=1)
+    # fig2.update_layout(title='Confirmed Cases',
+    #                yaxis_title='Confirmed',
+    #                xaxis_title='Pandemic')
+
+
+    # Bubble Plot Confirmed Deaths
+    # fig3 = go.Figure()
+    fig2.add_trace(go.Scatter(
+        x=names,
+        y=deaths,
+        mode='markers',
+        marker=dict(
+            size=size_deaths,
+            # color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)', 'rgb(255, 65, 104)']
+        ),
+        name="Confirmed Deaths"
+    ),row=1,col=2)
+    fig2.update_layout(width=800)
+    # fig3.update_layout(title='Confirmed Deaths',
+    #                yaxis_title='Deaths',
+    #                xaxis_title='Pandemic')
+
+    
+    fig3=go.Figure()
+    fig3.add_trace(go.Scatter(
+        x=names,
+        y=mortality_rate,
+        mode='markers',
+        marker=dict(
+            size=size_deaths,
+            # color=['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)', 'rgb(255, 65, 104)']
         )
     ))
+    fig3.update_layout(title="Mortality Rate",
+                    yaxis_title='Mortality Rate %',
+                   xaxis_title='Pandemic'
+    )
 
-    fig.update_layout(title='Confirmed Cases',
-                   yaxis_title='Confirmed',
-                   xaxis_title='Pandemic')
-
-    return fig
-
+    # print (covid.Confirmed.sum())
+    return fig,fig2,fig3
 
 
 
@@ -169,8 +235,6 @@ def plot_ebola(data):
 
 @st.cache
 def plot_swine_flu(swine_flu):
-    swine_flu["Date"]=pd.to_datetime(swine_flu["Update Time"])
-    swine_flu["Date"]=swine_flu["Date"].dt.date
     gb2=swine_flu.groupby(['Date']).sum()
 
     fig=go.Figure()
@@ -197,12 +261,14 @@ def main():
     pandemic=st.sidebar.selectbox("Pandemic",pandemics)
 
 
-    ebola,swine_flu,covid=load_datasets()
+    ebola,swine_flu,covid,covid_latest=load_datasets()
 
 
     if(pandemic=="All"):
-        chart=comparison(ebola,swine_flu,covid)
+        chart,chart2,chart3=comparison(ebola,swine_flu,covid,covid_latest)
         st.plotly_chart(chart)
+        st.plotly_chart(chart2)
+        st.plotly_chart(chart3)
     elif(pandemic=='Covid-19'):
         code=get_country_codes()
         country_select=st.sidebar.multiselect("Country",code.index)
